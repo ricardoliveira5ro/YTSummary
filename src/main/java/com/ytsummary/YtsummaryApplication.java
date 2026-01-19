58b13ef789b5c11e3ae1e2699be528682a8f9ad3
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-01-19 16:12:08 +0000

    first commit

---
+++ src/main/java/com/ytsummary/YtsummaryApplication.java
@@ -0,0 +1,13 @@
+package com.ytsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtsummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtsummaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ytsummary/YtsummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytsummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtsummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
