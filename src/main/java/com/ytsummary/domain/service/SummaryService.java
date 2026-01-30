9c4fcb962d9e927e94a52d45f616cb252269ea73
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-01-30 16:14:11 +0000

    integrate openAI model

--- src/main/java/com/ytsummary/domain/service/SummaryService.java
+++ src/main/java/com/ytsummary/domain/service/SummaryService.java
@@ -2,7 +2,9 @@ package com.ytsummary.domain.service;
 
 import com.ytsummary.domain.port.SummaryProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SummaryService {
 
     private SummaryProvider summaryProvider;
--- src/main/java/com/ytsummary/infrastructure/openai/OpenAISummaryProvider.java
+++ src/main/java/com/ytsummary/infrastructure/openai/OpenAISummaryProvider.java
@@ -1,11 +1,60 @@
 package com.ytsummary.infrastructure.openai;
 
 import com.ytsummary.domain.port.SummaryProvider;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
 public class OpenAISummaryProvider implements SummaryProvider {
+
+    private HttpClient httpClient;
+
+    @Value("${openai.api-token}")
+    private String openAIApiToken;
+
+    @Autowired
+    public void setHttpClient(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
 
     @Override
     public String getSummary(String transcript) {
+        JSONObject payload = new JSONObject()
+                .put("model", "gpt-4.1-nano")
+                .put("input", "This is a transcript from a youtube video, I want you to summarize it to understand what they are talking about without having to watch the whole video. " +
+                        "Please very concise and include any useful details and return only the summary and in plain text as paragraphs. Here is the transcript: `" + transcript + "`");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.openai.com/v1/responses"))
+                .header(HttpHeaders.CONTENT_TYPE,"application/json")
+                .header(HttpHeaders.AUTHORIZATION,"Bearer " + openAIApiToken)
+                .POST(HttpRequest.BodyPublishers.ofString(payload.toString()))
+                .build();
+
+        // To be parsed
+        String result = invokeRequest(request);
+
         return "";
+    }
+
+    private String invokeRequest(HttpRequest request) {
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() >= 400)
+                throw new RuntimeException("Something went wrong");
+
+            return response.body();
+        } catch (Exception e) {
+            throw new RuntimeException("HTTP call failed", e);
+        }
     }
 }
